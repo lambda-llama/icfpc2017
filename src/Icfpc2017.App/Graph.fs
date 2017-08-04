7a2ca627fbc059c1be1d5811be9bc4a485e13bf2
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-08-04 15:23:12 +0300

    Grow connected component strategy

--- src/Icfpc2017.App/Graph.fs
+++ src/Icfpc2017.App/Graph.fs
@@ -23,39 +23,42 @@ let isUnclaimedEdge edge = Option.isNone edge.Color
 
 let isEndPoint { Ends = (u, v) } vertex = vertex = u || vertex = v
 
+let outEdges graph vertex =
+    graph.Edges
+    |> List.filter (fun e -> isEndPoint e vertex)
 
 let empty = {
     NVerts = 0u;
     Edges = [];
 }
 
-let create verts edges = 
-    let newEdge e = { Ends = normalizeEdgeEnds e; Color = None} in 
+let create verts edges =
+    let newEdge e = { Ends = normalizeEdgeEnds e; Color = None} in
     { NVerts = verts; Edges = List.map newEdge edges; }
 
-let claimEdge graph color edge = 
+let claimEdge graph color edge =
     let claimed = normalizeEdgeEnds edge in
     let claim { Ends = ends; Color = c} =
-        if ends = claimed 
+        if ends = claimed
         then { Ends = ends; Color = Some color }
         else { Ends = ends; Color = c }
     in
     { NVerts = graph.NVerts; Edges = List.map claim graph.Edges }
 
-let private colors = [| 
-    "red"; "blue"; "pink"; "yellow"; "cyan"; "dimgrey"; "green"; "indigo" 
+let private colors = [|
+    "red"; "blue"; "pink"; "yellow"; "cyan"; "dimgrey"; "green"; "indigo"
 |]
 
-let toDot graph = 
-    let renderNode id = sprintf "  %d [label=\"%d\"]" id id in 
-    let renderEdge { Ends = (u, v); Color = c} = 
-        let color: string = 
-            match c with 
-            | Some(idx) -> Array.get colors (int idx) 
-            | None -> "black" 
+let toDot graph =
+    let renderNode id = sprintf "  %d [label=\"%d\"]" id id in
+    let renderEdge { Ends = (u, v); Color = c} =
+        let color: string =
+            match c with
+            | Some(idx) -> Array.get colors (int idx)
+            | None -> "black"
         in sprintf "  %d -- %d [color=\"%s\"]" u v color
     in
-    let mapJoin f xs = List.map f xs |> String.concat "\n" in 
+    let mapJoin f xs = List.map f xs |> String.concat "\n" in
     let nodes = [0u .. (graph.NVerts - 1u)] |> mapJoin renderNode in
-    let edges = graph.Edges |> mapJoin renderEdge in 
+    let edges = graph.Edges |> mapJoin renderEdge in
     sprintf "graph {\n%s\n%s\n}" nodes edges
--- src/Icfpc2017.App/Strategy.fs
+++ src/Icfpc2017.App/Strategy.fs
@@ -3,8 +3,8 @@ module Stragy
 type T = Game.State -> (Graph.VertexId * Graph.VertexId)
 
 let private maxByWeight (game: Game.State) (weight: Graph.Edge -> int) =
-    let { Graph.Ends = ends } = 
-        game.Graph.Edges 
+    let { Graph.Ends = ends } =
+        game.Graph.Edges
             |> List.filter Graph.isUnclaimedEdge
             |> List.maxBy weight
     in ends
@@ -12,6 +12,15 @@ let randomEdge: T = fun game ->
     let { Graph.Ends = ends } = game.Graph.Edges |> List.find Graph.isUnclaimedEdge in
     ends
 
-let growFromMines (game: Game.State) = 
-    let weight edge = if List.exists (Graph.isEndPoint edge)  game.Mines then 1 else 0 in 
+let growFromMines (game: Game.State) =
+    let attachedToMine edge = List.exists (Graph.isEndPoint edge) game.Mines in
+    let attachedToOurEdge { Graph.Ends = (u, v) } =
+        List.append (Graph.outEdges game.Graph u) (Graph.outEdges game.Graph u)
+        |> List.exists (fun {Graph.Color = c} ->
+            match c with
+            | Some(c) -> c = game.Me
+            | _ -> false
+        )
+    in
+    let weight edge = if attachedToMine edge || attachedToOurEdge edge then 1 else 0 in
     maxByWeight game weight
