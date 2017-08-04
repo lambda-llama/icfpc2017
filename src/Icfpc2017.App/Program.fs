3bb42389fcc3a386586d0e313619eb75c5819041
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-08-04 16:10:56 +0300

    example strategy

--- src/Icfpc2017.App/Program.fs
+++ src/Icfpc2017.App/Program.fs
@@ -19,6 +19,25 @@ let online () = async {
     return ()
 }
 
+let rec play g n = 
+    if n = 0
+        then [g]
+        else 
+            let (u, v) = Strategy.growFromMines g in
+            let g1 = Game.applyMoveIn g { move = { moves = [| ProtocolData.Claim { punter = 0; source = int u; target = int v }|] }; } in
+            g :: play g1 (n - 1)
+
 [<EntryPoint>]
 let main argv =
-    Async.RunSynchronously (online ()); 0
+    // Async.RunSynchronously (online ()); 0
+    let game =  """{"punter":0, "punters":2, "map":{"sites":[{"id":4},{"id":1},{"id":3},{"id":6},{"id":5},{"id":0},{"id":7},{"id":2}], "rivers":[{"source":3,"target":4},{"source":0,"target":1},{"source":2,"target":3}, {"source":1,"target":3},{"source":5,"target":6},{"source":4,"target":5}, {"source":3,"target":5},{"source":6,"target":7},{"source":5,"target":7}, {"source":1,"target":7},{"source":0,"target":7},{"source":1,"target":2}], "mines":[1,5]}}""" in
+    let setup = match ProtocolData.deserialize game with
+        | ProtocolData.MessageIn.Setup setup -> setup
+        | _ -> failwith ":("
+    in
+    let game = Game.initialState setup in
+    let games = play game 3 in
+    List.map (fun (g: Game.State) -> sprintf "%s" (Graph.toDot g.Graph)) games
+    |> String.concat "\n\n"
+    |> printf "%s"
+    0
