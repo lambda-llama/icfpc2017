d8385c18d796373931de5bb64ea3c268d5fd9df5
Aleksey Kladov <aleksey.kladov@gmail.com>
2017-08-04 19:12:12 +0300

    Fix the test

--- src/Icfpc2017.Tests/BFSTests.fs
+++ src/Icfpc2017.Tests/BFSTests.fs
@@ -6,12 +6,15 @@ open System.IO
 
 [<TestFixture>]
 module BFS = 
+    let vert id = {Graph.Id = id; Graph.IsSource = false; Graph.Coords = None}
+    let mine id = {Graph.Id = id; Graph.IsSource = true; Graph.Coords = None}
+
     [<Test>]
     let two_vertex_test (): unit =
         let nSites = 2u in
         let edges = [ (0u, 1u) ] in
-        let mines = [| 0u |] in 
-        let graph = Graph.create mines nSites edges in
+        let verts = [| mine 0u; vert 1u |] in
+        let graph = Graph.create verts edges in
         let distances = Array2D.init 1 2 (fun i j -> -1) in
         ShortestPath.BFS graph 0u distances;
         Assert.That(distances.[0,1], Is.EqualTo(1))
@@ -21,8 +24,8 @@ module BFS =
     let star_vertex_test (): unit =
         let nSites = 4u in
         let edges = [ (0u, 1u); (0u, 2u); (0u, 3u) ] in
-        let mines = [| 0u |] in 
-        let graph = Graph.create mines nSites edges in
+        let verts = [| mine 0u; vert 1u; vert 2u; vert 3u |] in 
+        let graph = Graph.create verts edges in
         let distances = Array2D.init 1 4 (fun i j -> -1) in
         ShortestPath.BFS graph 0u distances;
         Assert.That(distances.[0,1], Is.EqualTo(1))
@@ -34,8 +37,8 @@ module BFS =
     let long_worm (): unit =
         let nSites = 4u in
         let edges = [ (0u, 1u); (1u, 2u); (2u, 3u) ] in
-        let mines = [| 3u |] in 
-        let graph = Graph.create mines nSites edges in
+        let verts = [| vert 0u; vert 1u; vert 2u; mine 3u |] in 
+        let graph = Graph.create verts edges in
         let distances = Array2D.init 4 4 (fun i j -> -1) in
         ShortestPath.BFS graph 3u distances;
         Assert.That(distances.[3,0], Is.EqualTo(3))
