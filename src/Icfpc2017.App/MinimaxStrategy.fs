8c7f604c5f8f80b651fcc81927def75b3d0eebd8
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2017-08-05 20:19:02 +0200

    Port minimap to new graph (BROKEN)

--- src/Icfpc2017.App/MinimaxStrategy.fs
+++ src/Icfpc2017.App/MinimaxStrategy.fs
@@ -1,46 +1,52 @@
 module MinimaxStrategy
 
 open System
-open System.Collections.Generic
 open Game
-open Graph
-open Strategy
+open Graphs
+
+let isSource (graph: Graphs.Graph.T) vertex =
+    (Graphs.Graph.vertices graph).[vertex].IsSource
+
+let outEdges (graph: Graphs.Graph.T) vertex: Edge.T array =
+    (Graph.edges graph)
+    |> Array.filter (fun e -> Edge.contains e vertex)
+
+let isUnclaimedEdge (graph: Graphs.Graph.T) (edge: Edge.T): bool =
+    Graph.isClaimed graph edge |> not
 
 let isConnected (game: State) vertex: bool =
-    isSource game.Graph vertex ||
-        outEdges game.Graph vertex
-        |> List.exists (isUnclaimedEdge >> not)
+    isSource game.Graph2 vertex ||
+        outEdges game.Graph2 vertex
+        |> Array.exists (isUnclaimedEdge game.Graph2 >> not)
 
-let getUnclaimedEdges (game: State): Edge array =
-    game.Graph.Edges
-    |> List.filter isUnclaimedEdge
-    |> List.filter (fun { Ends = (a, b) } -> isConnected game a || isConnected game b)
-    |> List.toArray
+let getUnclaimedEdges (game: State): Edge.T array =
+    (Graphs.Graph.edges game.Graph2)
+    |> Array.filter (isUnclaimedEdge game.Graph2)
+    |> Array.filter
+        (fun e ->
+            let (a, b) = Edge.ends e
+            isConnected game a || isConnected game b)
 
 let heuristic (game: State): int =
-    let graph = game.Graph
-    let dists = ShortestPath.Compute graph
+    let graph = game.Graph2
+    let dists = Traversal.shortestPaths graph
     let reaches =
         [0..game.NumPlayers - 1]
         |> List.map
             (fun p ->
-                let isOwnedEdge { Graph.Color = cOpt } =
-                    match cOpt with
-                    | Some c -> c = p
-                    | None -> false
-                ShortestPath.Compute { graph with Graph.Edges = List.filter isOwnedEdge graph.Edges })
+                Traversal.shortestPaths (Graph.withEdges graph (Array.filter (Graph.isClaimedBy p graph) (Graph.edges graph))))
     let scores =
         reaches
-        |> List.map (fun r -> Game.score game dists r)
+        |> List.map (fun r -> Game.score2 game dists r)
         |> List.toArray
     let myScore = scores.[game.Me]
     scores.[game.Me] <- Int32.MinValue
     let bestOpponentScore = scores |> Array.max
     myScore - bestOpponentScore
 
-let rec _minimax (state: State) (edge: Edge) (player: Color) (depth: int) (alpha: int) (beta: int): int =
-    let { Ends = ends } = edge
-    let newState = { state with Graph = claimEdge state.Graph player ends }
+let rec _minimax (state: State) (edge: Edge.T) (player: Color) (depth: int) (alpha: int) (beta: int): int =
+    let eid = Edge.id edge
+    let newState = { state with Graph2 = Graph.claimEdge state.Graph2 player eid }
     let nextPlayer = (player + 1) % state.NumPlayers
     
     let min x y = if x < y then x else y
@@ -74,9 +80,9 @@ let rec _minimax (state: State) (edge: Edge) (player: Color) (depth: int) (alpha
 let minimax = 
     Strategy.stateless "minimax" (fun game ->
         let maxDepth = 10
-        let depth = int (Math.Min(maxDepth, game.Graph.Edges |> List.filter isUnclaimedEdge |> List.length))
+        let depth = int (Math.Min(maxDepth, (Graph.edges game.Graph2) |> Array.filter (isUnclaimedEdge game.Graph2) |> Array.length))
         getUnclaimedEdges game
         |> Array.sortByDescending (fun e -> _minimax game e game.Me depth Int32.MinValue Int32.MaxValue)
         |> Array.find (fun _ -> true)
-        |> fun { Ends = ends } -> ends
+        |> Edge.ends
     )
