ef86958c527b568db7d6f44416564e9237ec60c6
Sergei Lebedev <s.lebedev@criteo.com>
2017-08-05 23:31:47 +0200

    Uncommented TraversalTests

--- src/Icfpc2017.Tests/TraversalTests.fs
+++ src/Icfpc2017.Tests/TraversalTests.fs
@@ -1,20 +1,16 @@
 namespace Icfpc2017.Tests
 
 open NUnit.Framework
-open System.IO
 
 open Graphs
 
 [<TestFixture>]
 module TraversalTests =
-    let id = {IsSource=false; Coords=None}
-    let mine id = {IsSource=true; Coords=None}
-
     [<Test>]
     let singleEdge () =
         let edges = [|(0, 1)|] in
-        let nVertices = 2 in
-        let graph = Graph.create nVertices [|0|] edges in
+        let coords = Array.create 2 None in
+        let graph = Graph.create coords [|0|] edges in
         let source = Graph.sources graph |> Array.head
         let distances = Traversal.shortestPath graph source in
         Assert.That(distances.[1], Is.EqualTo 1)
@@ -23,8 +19,8 @@ module TraversalTests =
     [<Test>]
     let star () =
         let edges = [|(0, 1); (0, 2); (0, 3)|] in
-        let nVertices = 4 in
-        let graph = Graph.create nVertices [|0|] edges in
+        let coords = Array.create 4 None in
+        let graph = Graph.create coords [|0|] edges in
         let source = Graph.sources graph |> Array.head
         let distances = Traversal.shortestPath graph source in
         Assert.That(distances.[1], Is.EqualTo 1)
@@ -35,8 +31,19 @@ module TraversalTests =
     [<Test>]
     let longWorm () =
         let edges = [|(0, 1); (1, 2); (2, 3)|] in
-        let nVertices = 4 in
-        let graph = Graph.create nVertices [|3|] edges in
+        let coords = Array.create 4 None in
+        let graph = Graph.create coords [|3|] edges in
         let source = Graph.sources graph |> Array.last
         let distances = Traversal.shortestPath graph source in
         Assert.That(distances, Is.EqualTo [|3; 2; 1; 0|])
+
+    [<Test>]
+    let triangle () =
+        let edges = [|(0, 1); (1, 2); (2, 0)|] in
+        let coords = Array.create 3 None in
+        let graph = Graph.create coords [|0|] edges in
+        let source = Graph.sources graph |> Array.head
+        let distances = Traversal.shortestPath graph source in
+        Assert.That(distances.[0], Is.EqualTo 0)
+        Assert.That(distances.[1], Is.EqualTo 1)
+        Assert.That(distances.[2], Is.EqualTo 1)
