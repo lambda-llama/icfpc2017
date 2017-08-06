ae3a7259b6101ca405f1ff359aec53fa9015f4e3
Andrii Korol <a.korol@criteo.com>
2017-08-06 18:50:05 +0200

    Fix Union

--- src/Icfpc2017.App/Union.fs
+++ src/Icfpc2017.App/Union.fs
@@ -8,7 +8,7 @@ type T = {
 let getOneSideScore (u1: T) (u2: T) (dist: Map<int, int[]>) = 
     let mutable sum = 0 in
     for m in u1.Sources do
-        for s in u2.Sources do
+        for s in u2.Sites do
             let d = dist.[m].[int s] in
             sum <- sum + d * d
     sum
