647a958e5fecdc670ae4c36e96b6f6a975c38634
Eugene Kharitonov <e.kharitonov@criteo.com>
2017-08-06 15:14:01 +0200

    continuous tournament

--- src/tournament.py
+++ src/tournament.py
@@ -10,9 +10,10 @@ Room = namedtuple('Room', ['players_in', 'players_total', 'map_name', 'port'])
 port_pattern = re.compile("""<td>\w*</td><td>(9\d+)</td>""")
 status_pattern = re.compile("""(\d+)\/(\d+)""")
 map_pattern = re.compile("""/(\w+).json""")
+STRATEGY = None
 
-acc_wins = defaultdict(int)
-acc_games = defaultdict(int)
+wins = defaultdict(int)
+games = defaultdict(int)
 
 def get_rooms():
     page = urllib2.urlopen("http://punter.inf.ed.ac.uk/status.html").read()
@@ -27,8 +28,8 @@ def get_rooms():
 
         if status and port and map_name:
             room = Room(players_in=int(status.group(1)), players_total=int(status.group(2)), map_name=map_name.group(1), port=int(port.group(1)))
-            print room
             rooms.append(room)
+    print 'n rooms: ', len(rooms)
 
     return rooms
 
@@ -36,18 +37,18 @@ def get_score(port, strategy):
     command = ["dotnet", "Icfpc2017.App/bin/Release/netcoreapp2.0/Icfpc2017.App.dll", str(port), strategy]
     try:
         s = subprocess.check_output(command, stderr=subprocess.STDOUT)
-        print s
+        #print s
         meta = ast.literal_eval(s.splitlines()[-1])
         return meta["me"], meta["scores"]
     except Exception as e:
         print "FAILED", command
         return None
 
-def print_stats():
-    for strategy in acc_games:
-        print "strategy: {}".format(strategy)
-        print "\ttotal games: {}".format(acc_games[strategy])
-        print "\twins: {}".format(acc_wins[strategy])
+def print_stats(strategy):
+    for map_name in games:
+        print "strategy: {}, map: {}".format(strategy, map_name)
+        print "\ttotal games: {}".format(games[map_name])
+        print "\twins: {}".format(wins[map_name])
 
 def thread_func(strategy, room):
     result = get_score(room.port, strategy)
@@ -55,28 +56,37 @@ def thread_func(strategy, room):
     if result is not None:
         me, scores = result
         print me, scores
-        acc_games[strategy] += 1
-        acc_wins[strategy] += 1 if scores[me] == max(scores) else 0
-        print_stats()
+        games[room.map_name] += 1
+        wins[room.map_name] += 1 if scores[me] == max(scores) else 0
+        print_stats(strategy)
 
 if __name__ == '__main__':
     import sys
     import random
     import time
-    n_threads = 10
+    n_threads = 20
 
-    strategy = 'minimax2'
-
-    rooms = get_rooms()
+    strategy = sys.argv[1]
     workers = []
 
-    for room in rooms:
-        if room.players_total - 1 == room.players_in and n_threads > 0:
-            worker = Thread(target=thread_func, args=(strategy, room))
-            worker.daemon = True
-            worker.start()
-            workers.append(worker)
-            n_threads -= 1
+    while True:
+        rooms = get_rooms()
+        random.shuffle(rooms)
+        spawned_threads = 0
 
-    for w in workers:
-        w.join()
+        for room in rooms:
+            if room.players_in + 1 == room.players_total and spawned_threads < n_threads:
+                worker = Thread(target=thread_func, args=(strategy, room))
+                worker.daemon = True
+                worker.start()
+                workers.append(worker)
+                spawned_threads += 1
+
+        time.sleep(10)
+        remaining_workers = []
+        for w in workers:
+            w.join(1)
+            if w.isAlive():
+                remaining_workers.append(w)
+        print 'remaining {} workers'.format(len(remaining_workers))
+        workers = remaining_workers
