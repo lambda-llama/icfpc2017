e3a76340a5fafb4e3ddd254ac2cbdbfb94bbb872
Sergei Lebedev <s.lebedev@criteo.com>
2017-08-06 23:12:24 +0200

    Some Graph tests

--- src/Icfpc2017.Tests/GraphTests.fs
+++ src/Icfpc2017.Tests/GraphTests.fs
@@ -7,10 +7,39 @@ open Graphs
 module GraphTests =
     [<Test>]
     let edgeId () =
-        let edges = [|(0, 1)|]
-        let graph = Graph.testCreate 2 [|0|] edges
+        let graph = Graph.testCreate 2 [|0|] [|(0, 1)|]
         Assert.That(Graph.edgeId graph (0, 1), Is.EqualTo 0)
         Assert.That(Graph.edgeId graph (1, 0), Is.EqualTo 0)
+
+    [<Test>]
+    let sourcesSinks () =
+        let graph = Graph.testCreate 4 [|0; 2|] [|(0, 1); (1, 3)|]
+        Assert.That (Graph.sources graph, Is.EqualTo <| seq [0; 2])
+        Assert.That (Graph.sinks graph, Is.EqualTo <| seq [1; 3])
+
+    [<Test>]
+    let nEdges () =
+        let edges = [|(0, 1); (1, 3)|]
+        let graph = Graph.testCreate 4 [|0; 2|] edges
+        Assert.That (Graph.nEdges graph, Is.EqualTo edges.Length)
+
+    [<Test>]
+    let claiming () =
+        let graph = Graph.testCreate 4 [|0; 2|] [|(0, 1); (1, 3)|]
+        let edge = Graph.unclaimed graph |> Seq.head
+        Assert.False (Graph.isClaimed graph edge)
+        Assert.False (Graph.isClaimedBy 42 graph edge)
+        Assert.That (Graph.edgeColor graph edge, Is.EqualTo None)
+
+        let graph2 = Graph.claimEdge graph 42 (Edge.id edge)
+        Assert.True (Graph.isClaimed graph2 edge)
+        Assert.True (Graph.isClaimedBy 42 graph2 edge)
+        Assert.That (Graph.claimedBy graph2 42, Is.EqualTo <| seq [edge])
+
+    [<Test>]
+    let adjacent () =
+        let graph = Graph.testCreate 4 [|0; 2|] [|(0, 1); (0, 2); (1, 3)|]
+        Assert.That (Graph.adjacent graph 0, Is.EqualTo <| seq [1; 2])
 
 module TraversalTests =
     [<Test>]
