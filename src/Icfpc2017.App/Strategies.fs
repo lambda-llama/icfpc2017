8ff5093e0762fa5d185b742eef9244cf885e6a7d
Andrii Korol <a.korol@criteo.com>
2017-08-06 17:16:00 +0200

    Add GreadyStrategy to the list of Strategies

--- src/Icfpc2017.App/Strategies.fs
+++ src/Icfpc2017.App/Strategies.fs
@@ -3,7 +3,7 @@ module Strategies
 open BruteForceStrategy
 open GrowFromMinesStrategy
 open MinimaxStrategy
-// open GreadyStrategy
+open GreadyStrategy
 open MixedStrategy
 
 let all =
@@ -12,7 +12,7 @@ let all =
      (bruteForce3.name, bruteForce3);
      (minimax.name, minimax);
      (minimax2.name, minimax2);
-     // (greadyStrategy.name, greadyStrategy);
+     (greadyStrategy.name, greadyStrategy);
      (mixedStrategy.name, mixedStrategy)
      (combinedForce.name, combinedForce)]
     |> Map.ofList
