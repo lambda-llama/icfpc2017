4cbe1a2d026f66771b05912ddcc0304f51b8dd54
Eugene Kharitonov <e.kharitonov@criteo.com>
2017-08-06 16:35:24 +0200

    more adv tournament

--- src/tournament.py
+++ src/tournament.py
@@ -6,7 +6,7 @@ import subprocess
 from threading import Thread
 from collections import namedtuple, defaultdict
 
-Room = namedtuple('Room', ['players_in', 'players_total', 'map_name', 'port'])
+Room = namedtuple('Room', ['players_in', 'players_total', 'map_name', 'port', 'enemies'])
 port_pattern = re.compile("""<td>\w*</td><td>(9\d+)</td>""")
 status_pattern = re.compile("""(\d+)\/(\d+)""")
 map_pattern = re.compile("""/(\w+).json""")
@@ -15,21 +15,29 @@ STRATEGY = None
 wins = defaultdict(int)
 games = defaultdict(int)
 
+def get_enemies(s):
+    # UGLY AS MY LIFE
+    p1 = s.find(")</td><td>")
+    s = s[p1 + 10:]
+    p2 = s.find("""</td><td></td><td>""")
+    s = s[:p2]
+    return map(lambda x: x.strip(), s.split(","))
+
 def get_rooms():
     page = urllib2.urlopen("http://punter.inf.ed.ac.uk/status.html").read()
 
     rooms = []
-
     lines = page.split("\n")
     for line in lines:
+        if 'lambda-llama' in line:
+            continue
         status = re.search(status_pattern, line)
         port   = re.search(port_pattern, line)
         map_name = re.search(map_pattern, line)
-
+        enemies = get_enemies(line)
         if status and port and map_name:
-            room = Room(players_in=int(status.group(1)), players_total=int(status.group(2)), map_name=map_name.group(1), port=int(port.group(1)))
+            room = Room(players_in=int(status.group(1)), players_total=int(status.group(2)), map_name=map_name.group(1), port=int(port.group(1)), enemies=enemies)
             rooms.append(room)
-    print 'n rooms: ', len(rooms)
 
     return rooms
 
@@ -45,26 +53,26 @@ def get_score(port, strategy):
         return None
 
 def print_stats(strategy):
-    for map_name in games:
-        print "strategy: {}, map: {}".format(strategy, map_name)
-        print "\ttotal games: {}".format(games[map_name])
-        print "\twins: {}".format(wins[map_name])
+    for key, total_games in games.iteritems():
+        enemy, map_name = key
+        print "strategy: {}, enemy: {}, map: {}".format(strategy, enemy, map_name)
+        print "\ttotal games: {}".format(total_games)
+        print "\twins: {}".format(wins[key])
 
 def thread_func(strategy, room):
     result = get_score(room.port, strategy)
-    print result
+    #print result
     if result is not None:
         me, scores = result
-        print me, scores
-        games[room.map_name] += 1
-        wins[room.map_name] += 1 if scores[me] == max(scores) else 0
-        print_stats(strategy)
+        for enemy in room.enemies:
+            games[(enemy, room.map_name)] += 1
+            wins[(enemy, room.map_name)] += 1 if scores[me] == max(scores) else 0
 
 if __name__ == '__main__':
     import sys
     import random
     import time
-    n_threads = 20
+    n_threads = 50
 
     strategy = sys.argv[1]
     workers = []
@@ -75,7 +83,7 @@ if __name__ == '__main__':
         spawned_threads = 0
 
         for room in rooms:
-            if room.players_in + 1 == room.players_total and spawned_threads < n_threads:
+            if room.players_in > 0 and spawned_threads < n_threads:
                 worker = Thread(target=thread_func, args=(strategy, room))
                 worker.daemon = True
                 worker.start()
@@ -90,3 +98,5 @@ if __name__ == '__main__':
                 remaining_workers.append(w)
         print 'remaining {} workers'.format(len(remaining_workers))
         workers = remaining_workers
+        print_stats(strategy)
+
